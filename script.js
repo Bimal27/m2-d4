141d5b16213807e72c1e2199a56f15d9668ebb2e
Bimal27 <bimalsah1996@gmail.com>
2021-06-11 18:52:05 +0200

    updated

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ol = document.querySelector("ol");
 
+
 function inputLength() {
     return input.value.length;
 }
@@ -16,4 +17,28 @@ button.addEventListener("click", function () {
     }
 
 })
+
+
+var button2 = document.getElementById("button-add2");
+var input2 = document.getElementById("numberOfTeams");
+var ul = document.querySelector("ul")
+
+
+button2.addEventListener("click", function () {
+    let li = document.createElement("li");
+    li.className = "number-of-team";
+    li.textContent = input2.value
+    ul.appendChild(li)
+
+
+
+})
+
+
+//let assign = document.querySelectorAll('assign')
+
+//function numberOfTeams() {
+    //return ol.length;//
+
+//}//
 
